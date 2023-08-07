168b3e24fb883d6b6958ae4374e77f9836ca4e07
jvg87 <jvg87.dev@gmail.com>
2023-08-07 16:06:15 -0300

    feat: creating products list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,84 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, FlatList } from 'react-native';
 
 import { Feather } from '@expo/vector-icons'
 
+import Product from '../../components/Products';
+
 export default function Home() {
   const navigation = useNavigation();
+
+  const [products, setProducts] = useState([
+    {
+      id: '1',
+      name: 'Coca Cola',
+      price: 19.90
+    }, 
+    {
+      id: '2',
+      name: 'Chocolate',
+      price: 6.50
+    },
+    {
+      id: '3',
+      name: 'Queijo 500g',
+      price: 15
+    },
+    {
+      id: '4',
+      name: 'Batata frita',
+      price: 23.90
+    },
+    {
+      id: '5',
+      name: 'Guarana Lata',
+      price: 6.00
+    },
+    {
+      id: '6',
+      name: 'Biscoito',
+      price: 3.90
+    },
+    {
+      id: '7',
+      name: 'Pão de  forma',
+      price: 5.90
+    },
+    {
+      id: '8',
+      name: 'Suco',
+      price: 3
+    },
+    {
+      id: '9',
+      name: 'Pão Francês',
+      price: 2.50
+    },
+    {
+      id: '10',
+      name: 'Achocolatado',
+      price: 12
+    },
+  ]);
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
         <Text style={styles.title}>Lista de Produtos</Text>
-        <TouchableOpacity style={styles.cartButton}>
+        <TouchableOpacity style={styles.cartButton} onPress={() => navigation.navigate('Cart')}>
           <View style={styles.dot}>
             <Text style={styles.dotText}>3</Text>
           </View>
           <Feather name='shopping-cart' size={30} color='#000'/>
         </TouchableOpacity>
       </View>
-      
+      <FlatList
+        style={styles.list}
+        data={products}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({item}) => <Product data={item}/>}
+      />
     </SafeAreaView>
   )
 }
